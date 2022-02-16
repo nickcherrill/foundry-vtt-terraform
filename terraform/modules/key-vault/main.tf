76dbb25657f5868e08b6e44ab9ef7aabde2a08f8
nickcherrill <nickcherrill@users.noreply.github.com>
2022-02-16 12:52:36 +0000

    add resource modules, reference state storage

---
+++ terraform/modules/key-vault/main.tf
@@ -0,0 +1,6 @@
+# Deploy a Key Vault
+#
+# For suggested naming conventions, refer to:
+#   https://docs.microsoft.com/en-us/azure/cloud-adoption-framework/ready/azure-best-practices/naming-and-tagging
+
+# Key vault ##TODO
